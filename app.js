fd77b54933839b5d923f3388e430da45ee8c18d8
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2025-12-24 23:39:13 +0500

    add functionality

---
+++ app.js
@@ -0,0 +1,19 @@
+let userContainer = document.querySelector('.userContainer')
+
+const arr = [
+    {
+        profileUrl: '2wCEAAkGBxMSEhUSEhMVFRUVFRUVFRUVFRUVFRUVFRUWFhUVFRUYHSggGBolHRUVITEhJSkrLi4uFx8zODMtNygtLisBCgoKDg0OFxAQFy0dHR0tLS0rKy0tLS0tKy0tLSstLS0tLS0rLS0tKy0tLS0tLS0tLS0rKy0tKy0tLS0rKy0rLf',
+        name: 'Bruce Wayne',
+        email: 'bruce@wayne.com'
+    },
+     {
+        profileUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQFAYpDi8rMTvjC99bjqEkphlyNzIBvLLfIg&s',
+        name: 'Levi Ackerman',
+        email: 'levi@ackerman.com'
+    },
+     {
+        profileUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEgCXTeeoBphZcsGL7fIPUYkbBZTWAwNz8Ug&s',
+        name: 'Lelouch vi britannia',
+        email: 'lelouch@britannia.com'
+    },
+]
